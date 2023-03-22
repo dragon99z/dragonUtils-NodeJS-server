f2bd8c35a8cfd48cedba7e7451903b3dce8d7e3c
dragon99z <dragon99@online.de>
2023-03-22 03:54:43 +0100

    Update

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+const rateLimit = require('express-rate-limit')
+const session = require('express-session');
+const csurf = require('csurf');
 const express = require("express");
 const https = require('https');
 const fs = require("fs");
-const app = express();
+let app = express();
+app.use("/events",rateLimit());
 app.use(require('sanitize').middleware);
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: "notagoodsecret",
+    cookie: {httpOnly: true,secure: true}
+}));
+app.use(csurf());
+app.disable('x-powered-by');
+app.set('host', process.env.IP || '127.0.0.1');
+app.set('port', process.env.PORT || 3000);
 const port = 3000;
 
 
